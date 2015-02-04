e58bd22ed4953dc5e0d9b4fab940d2ce02f69ec5
Clay Baenziger <cbaenziger@bloomberg.net>
2015-02-04 13:04:58 -0500

    Leave breadcrumb to Chef-BACH

--- cookbooks/bcpc-hadoop/recipes/pig.rb
+++
@@ -1,5 +0,0 @@
-%w{pig pig-udf-datafu}.each do |pkg|
-  package pkg do
-    action :upgrade
-  end
-end
--- vbox_save.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-for i in `seq 1 3`; do
-  VBoxManage snapshot bcpc-vm$i take initial-install
-done
